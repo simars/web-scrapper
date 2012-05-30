1c807f43166f0d5c437c30cad8ff0769ffeb8c84
Simar Paul Singh <code@ssimar.com>
2012-05-30 14:28:34 -0400

    rescue from exception on parsing every item, print and continue

--- app/scrapers/just_dial_scrapper.rb
+++ app/scrapers/just_dial_scrapper.rb
@@ -17,21 +17,25 @@ module JustDialScrapper
     item_collection = []
     doc = Nokogiri::HTML(open(location))
     doc.css('.Ctitle > a').each do |element|
-      item = Item.new
-      item.source=location
-      item.link = element.attributes['href'].value rescue "error"
-      item.link = item.link.gsub!('<','3C').gsub('>','3E')
-      item.name = element.text;
-      itemDoc = Nokogiri::HTML(open(item.link))
-      if item.link
-        item.contact = itemDoc.at_css('.contName + span').text rescue "error"
-        item.phone =  itemDoc.at_css('.contPhone + span').text rescue "error"
-        item.address = itemDoc.at_css('.contAdd + span').gsub!('View Map | Locate on Map','').text rescue "error"
+      begin
+        item = Item.new
+        item.source=location
+        item.link = element.attributes['href'].value rescue "error"
+        item.link = item.link.gsub!('<','3C').gsub('>','3E')
+        item.name = element.text;
+        itemDoc = Nokogiri::HTML(open(item.link))
+        if item.link
+          item.contact = itemDoc.at_css('.contName + span').text rescue "error"
+          item.phone =  itemDoc.at_css('.contPhone + span').text rescue "error"
+          item.address = itemDoc.at_css('.contAdd + span').gsub!('View Map | Locate on Map','').text rescue "error"
+        end
+        if block_given?
+          item = yield item
+        end
+        item_collection << item
+      rescue exception => e
+        puts e
       end
-      if block_given?
-        item = yield item
-      end
-      item_collection << item
     end
     item_collection
   end
